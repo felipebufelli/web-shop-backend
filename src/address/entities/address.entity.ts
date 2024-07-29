3a5a01a4cd4195ffe24b5165a2423039f13f011c
DESKTOP-P23HATG\felip <felipe.bufelli@gmail.com>
2024-07-29 17:44:32 -0300

    Create address service and controller

--- src/address/entities/address.entity.ts
+++ src/address/entities/address.entity.ts
@@ -1,28 +1,33 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
-@Entity({name: 'address'})
+@Entity({ name: 'address' })
 export class AddressEntity {
-    @PrimaryGeneratedColumn('rowid')
-    id: number;
+  @PrimaryGeneratedColumn('rowid')
+  id: number;
 
-    @Column({name: 'user_id', nullable: false})
-    userId: number;
+  @Column({ name: 'user_id', nullable: false })
+  userId: number;
 
-    @Column({name: 'complement', nullable: true})
-    complement: string;
-    
-    @Column({name: 'number', nullable: false})
-    number_address: number;
-    
-    @Column({name: 'cep', nullable: false})
-    cep: string;
-    
-    @Column({name: 'city_id', nullable: false})
-    cityId: number;
-    
-    @CreateDateColumn({name: 'created_at'})
-    createdAt: Date;
+  @Column({ name: 'complement', nullable: true })
+  complement: string;
 
-    @CreateDateColumn({name: 'updated_at'})
-    updateAt: Date;
+  @Column({ name: 'number', nullable: false })
+  numberAddress: number;
+
+  @Column({ name: 'cep', nullable: false })
+  cep: string;
+
+  @Column({ name: 'city_id', nullable: false })
+  cityId: number;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @CreateDateColumn({ name: 'updated_at' })
+  updateAt: Date;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,23 +1,29 @@
-import { Body, Controller, Get, Post} from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UserService } from './user.service';
-
+import { ReturnUserDto } from './dtos/returnUser.dto';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
 
-    constructor(private readonly userService: UserService) {
+  @UsePipes(ValidationPipe)
+  @Post()
+  async createUser(@Body() createUser: CreateUserDto) {
+    return this.userService.create(createUser);
+  }
 
-    }
-    
-    @Post()
-    async createUser(@Body() createUser: CreateUserDto) {
-        return this.userService.create(createUser);
-    }
-
-    @Get()
-    async getAllUsers() {
-        return this.userService.findAll();
-    }
-
+  @Get()
+  async getAllUsers(): Promise<ReturnUserDto[]> {
+    return (await this.userService.findAll()).map(
+      (obj) => new ReturnUserDto(obj),
+    );
+  }
 }
