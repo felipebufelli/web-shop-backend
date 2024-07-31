be02ae8c74e7acd612e55adfd7e8f53beba4733a
DESKTOP-P23HATG\felip <felipe.bufelli@gmail.com>
2024-07-31 16:50:25 -0300

    Modificated DTOs of City and State

--- src/city/dtos/returnCity.dto.ts
+++ src/city/dtos/returnCity.dto.ts
@@ -2,12 +2,10 @@ import { ReturnStateDto } from 'src/state/dtos/returnState.dto';
 import { CityEntity } from '../entities/city.entity';
 
 export class ReturnCityDto {
-  id: number;
   name: string;
   state?: ReturnStateDto;
 
   constructor(city: CityEntity) {
-    this.id = city.id;
     this.name = city.name;
     this.state = city.state ? new ReturnStateDto(city.state) : undefined;
   }
--- src/state/dtos/returnState.dto.ts
+++ src/state/dtos/returnState.dto.ts
@@ -1,11 +1,9 @@
-import { StateEntity } from "../entities/state.entity";
+import { StateEntity } from '../entities/state.entity';
 
 export class ReturnStateDto {
-  id: number;
   name: string;
 
   constructor(state: StateEntity) {
-    this.id = state.id;
     this.name = state.name;
   }
 }
